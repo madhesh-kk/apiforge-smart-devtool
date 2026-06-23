4bd6477a6c3a19065067fd99967cc076504942e9
VICKNESHWARAN K <mmadhesh379@example.com>
2026-06-23 15:56:12 +0530

    Init Spring Boot project structure with Maven

---
+++ src/main/java/com/apiforge/ApiForgeApplication.java
@@ -0,0 +1,12 @@
+package com.apiforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiForgeApplication.class, args);
+    }
+}
